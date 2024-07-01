fa5e8ec537d54f90bdb792494930dcbac3a0e228
Kar-ho Lee <k-h_lee@hotmail.co.uk>
2024-07-01 14:47:31 +0100

    work on login feature

--- src/test/java/com/sparta/swaggerlabstests/lib/pages/LoginPage.java
+++ src/test/java/com/sparta/swaggerlabstests/lib/pages/LoginPage.java
@@ -7,6 +7,8 @@ public class LoginPage extends BasePage {
     private String username;
     private String password;
     private By loginButton = By.id("login-button");
+    private By loginName = By.id("user-name");
+    private By loginPass = By.id("password");
 
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
@@ -14,7 +16,7 @@ public class LoginPage extends BasePage {
 
     @Override
     protected boolean isCorrectPage() {
-        return webDriver.getCurrentUrl().equals("https://www.saucedemo.com/v1/");
+        return webDriver.getCurrentUrl().equals("https://www.saucedemo.com/v1/index.html");
     }
 
     public void setUsername(String username) {
@@ -25,8 +27,15 @@ public class LoginPage extends BasePage {
         this.password = password;
     }
 
-    public void clickLogin() {
+    public void enterLoginDetails(){
+        webDriver.findElement(loginName).sendKeys(username);
+        webDriver.findElement(loginPass).sendKeys(password);
+    }
+
+    public WebDriver clickLogin() {
+        enterLoginDetails();
         webDriver.findElement(loginButton).click();
+        return webDriver;
     }
 
 }
--- src/test/java/com/sparta/swaggerlabstests/stepdefs/LoginStepDefs.java
+++ src/test/java/com/sparta/swaggerlabstests/stepdefs/LoginStepDefs.java
@@ -1,13 +1,19 @@
 package com.sparta.swaggerlabstests.stepdefs;
 
 import com.sparta.swaggerlabstests.lib.pages.LoginPage;
+import com.sparta.swaggerlabstests.lib.pages.ProductsPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+
+import static org.hamcrest.Matchers.is;
 
 public class LoginStepDefs extends StepDefsSuper{
     LoginPage loginPage;
+    ProductsPage productsPage;
 
     @Given("I am on the login page")
     public void iAmOnTheLoginPage() {
@@ -15,7 +21,7 @@ public class LoginStepDefs extends StepDefsSuper{
         loginPage = new LoginPage(webDriver);
     }
 
-    @When("I Input a valid Username and Password")
+    @When("I input a valid username and password")
     public void iInputAValidUsernameAndPassword() {
         loginPage.setUsername("standard_user");
         loginPage.setPassword("secret_sauce");
@@ -28,5 +34,11 @@ public class LoginStepDefs extends StepDefsSuper{
 
     @Then("I will be taken to the Products Page")
     public void iWillBeTakenToTheProductsPage() {
+//        MatcherAssert.assertThat(productsPage.getUrl(), is("https://www.saucedemo.com/v1/inventory.html"));
     }
+
+
+//    @When("I login with the following details")
+//    public void iLoginWithTheFollowingDetails() {
+//    }
 }
